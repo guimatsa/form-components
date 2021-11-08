3ffb162cbd71e381298ffd6e867a631c276e2827
guimatsa <julio.guimatsa@insightahead.com>
2021-11-08 11:49:11 +0100

    tirth com

--- form-components/src/App.js
+++ form-components/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-dom';
+
+import MasterForm2 from './components/test';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route exact path='/MasterForm2' component={MasterForm2} />
+              
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
